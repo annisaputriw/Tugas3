6682cdb31cac541e08d5c9429c0d68c623ffeb91
annisaputriw <2310631230030@student.unsika.ac.id>
2024-09-25 14:42:11 +0700

    tugas.py

--- tugas.py
+++ tugas.py
@@ -38,6 +38,7 @@ class Blockchain:
         Mengembalikan blok terakhir di dalam blockchain\
         """
         return self.chain[-1]
+        
     def add_block(self, new_block):
         """
         Menambahkan blok baru ke dalam blockchain setelah memvalidasi proof of work
@@ -45,3 +46,51 @@ class Blockchain:
         new_blocl.previous_hash = self.get_latest_block().hash
         new_block.hash = self.proof_of_work(new_block)
         self.chain.append(new_block)
+    
+     def proof_of_work(self,block):
+         """
+         Proses Penambangan sederhana yang mencoba menemukan hash yang valid (dengan kesulitan tertentu).
+         """
+         block.nonce = 0
+         calculated_hash = block.calculated_hash()
+         while not calculated_hash.startswith('0' * self.difficulty):
+            block.nonce += 1
+            calculated_hash = block.calculate_hash()
+        return calculated_hash
+
+    def is_chain_valid(self):
+        """
+        Memeriksa validitas blockchain dengan memverifikasi setiap blok dan hash yang saling merujuk.
+        """
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i - 1]
+
+            # Periksa apakah hash blok saat ini benar
+            if current_block.hash != current_block.calculate_hash():
+                print(f"Hash of block {i} is invalid!")
+                return False
+
+ # Periksa apakah blok ini merujuk ke hash blok sebelumnya dengan benar
+            if current_block.previous_hash != previous_block.hash:
+                print(f"Previous hash of block {i} is invalid!")
+                return False
+        
+        return True
+
+# Demonstrasi
+
+# Membuat blockchain baru
+my_blockchain = Blockchain()
+
+# Menambah beberapa blok baru ke dalam blockchain
+my_blockchain.add_block(Block(1, "", time.time(), "Transaksi 1"))
+my_blockchain.add_block(Block(2, "", time.time(), "Transaksi 2"))
+my_blockchain.add_block(Block(3, "", time.time(), "Transaksi 3"))
+
+# Cetak seluruh blok di dalam blockchain
+for block in my_blockchain.chain:
+    print(block)
+
+# Memeriksa apakah blockchain valid
+print(f"Apakah blockchain valid? {my_blockchain.is_chain_valid()}")
